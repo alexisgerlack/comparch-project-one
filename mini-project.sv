b34c99dae5c8cd0edaccb73893ff7fbf2fced770
Lex <agerlack@olin.edu>
2026-09-14 15:28:47 -0400

    beginning project logic

---
+++ mini-project.sv
@@ -0,0 +1,13 @@
+module top(RGB_B,RGB_G,RGB_R, PWM_INTERVAL,clk);
+    output logic  RGB_B, RGB_R, RGB_G;
+    parameter PWM_INTERVAL = 1200;
+    logic clk = 0;
+
+    always@(posedge clk);
+    begin
+    assign RGB_R = 
+    assign RBG_B = 
+    assign RGB_G = SW;
+    end
+
+endmodule
