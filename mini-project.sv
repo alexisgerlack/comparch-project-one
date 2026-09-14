299c25ebe91f5bed8258fe611b574200c18a3e3d
Lex <agerlack@olin.edu>
2026-09-14 15:51:06 -0400

    added makefile and updated logic

--- mini-project.sv
+++ mini-project.sv
@@ -2,12 +2,10 @@ module top(RGB_B,RGB_G,RGB_R, PWM_INTERVAL,clk);
     output logic  RGB_B, RGB_R, RGB_G;
     parameter PWM_INTERVAL = 1200;
     logic clk = 0;
+    int color_one[3] = '{1,0,0};
 
-    always@(posedge clk);
-    begin
-    assign RGB_R = 
-    assign RBG_B = 
-    assign RGB_G = SW;
+    always@(posedge clk) begin
+    assign RGB_R = color_one[0];
     end
 
 endmodule
